e700bbe77c5d00b34d35b7300f8e001c84608d41
Rafael Tavares <rafael.tavares.informatica@gmail.com>
2026-06-01 16:30:13 -0300

    Ajuste na largura dos componentes do painel do admin

--- cefet-frontend/src/pages/Admin.jsx
+++ cefet-frontend/src/pages/Admin.jsx
@@ -383,7 +383,7 @@ const Admin = () => {
 
       <div className="flex-1 min-w-0 flex flex-col pb-16 md:pb-0 overflow-y-auto h-screen">
 
-        <div className="px-4 md:px-8 py-6 max-w-5xl w-full mx-auto">
+        <div className="px-4 md:px-8 py-6 max-w-7xl w-full mx-auto">
 
 
           {/* ======================================================
